bdf90a917f7cbc9fc78ccd4afb744793e2d9886e
Fai <faiabdulla123@gmail.com>
2024-02-07 01:11:59 +0300

    lap1

---
+++ lab1.py
@@ -0,0 +1,16 @@
+Price=4.99
+quantity=4
+tax_rate=7.5
+subtotal=Price*quantity
+tax=subtotal*tax_rate
+total=subtotal+tax
+print("Price of item: $",Price)
+print("Quantity:",quantity)
+print("Tax rate: %",tax_rate)
+print("Subtotal: $",subtotal)
+print("Tax: $",tax)
+print("Totai: $",total)
+
+
+
+#Bonus
