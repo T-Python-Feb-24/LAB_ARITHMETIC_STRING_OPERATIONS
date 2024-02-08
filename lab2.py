fdd8c0c9c7677c8f4fecf1ec108b0b2671a5a846
jmailnaif <jamailotb15@gmail.com>
2024-02-08 21:02:00 +0300

    your message

---
+++ lab2.py
@@ -0,0 +1,11 @@
+price = 2.99
+quantity = 3
+taxt_rate =  7,5 
+operation = price * quantity 
+subtotal = operation
+print(subtotal)
+txt = (subtotal * taxt_rate)
+print(txt)
+total = operation + txt
+print (total)
+print (subtotal + txt + total)
