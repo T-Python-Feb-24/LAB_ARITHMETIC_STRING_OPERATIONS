799396c7a5d26644635fbb00da0c43ae3b0c7e26
jmailnaif <jamailotb15@gmail.com>
2024-02-09 11:31:36 +0300

    bonus

---
+++ bonus.py
@@ -0,0 +1,8 @@
+my_string :str = " hello  i am jmail naif from saudi i am studant in tuwaiq"
+print(my_string. upper() )
+print(my_string[1:6])
+print(my_string. count("i"))
+print(my_string.title())
+print(my_string.lower())
+print(my_string.replace("saudi" , "kuwait"))
+print(my_string[-6:])
